86f98f222e45639cc65eb1fb35bb0d80b38e2f62
Iskra B <ibatistapoblete@gmail.com>
2019-04-22 03:35:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash[:yukihiro_matsumoto][known_for: "Ruby"][languages: ["LISP", "C"]]
 
 	programmer_hash =
  		{
@@ -82,7 +81,7 @@ programmer_hash[:yukihiro_matsumoto][known_for: "Ruby"][languages: ["LISP", "C"]
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto][known_for: "Ruby"][languages: ["LISP", "C"]]
 
 end
 
